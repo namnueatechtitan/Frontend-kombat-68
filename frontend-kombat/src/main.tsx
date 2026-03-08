aeca16eb8a279732af255d049ae9014c3e914ebc
namnueatechtitan <techtitannamnuea@gmail.com>
2026-03-09 03:33:22 +0700

    commit

--- frontend-kombat/src/main.tsx
+++ frontend-kombat/src/main.tsx
@@ -2,6 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import logoUrl from './assets/images/logo.png'
+
+const favicon = document.querySelector<HTMLLinkElement>("link[rel='icon']")
+
+if (favicon) {
+  favicon.href = logoUrl
+} else {
+  const link = document.createElement("link")
+  link.rel = "icon"
+  link.type = "image/png"
+  link.href = logoUrl
+  document.head.appendChild(link)
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
